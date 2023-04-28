5be4b6212ea3bdbd73a9e51be4da9d2e44277030
nieveslnormanss@gmail.com <>
2023-04-28 12:22:48 +0800

    Camera.main.ScreenPointToRay

---
+++ Assets/ScreenClicker.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenClicker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Fire2"))
+            Clicked ();
+        
+    }
+
+    void Clicked()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        RaycastHit hit = new RaycastHit();
+
+        if(Physics.Raycast(ray,out hit)) {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
