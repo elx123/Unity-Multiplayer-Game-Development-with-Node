6314c050ca12ccfc11b78e73fb8787c1b7c4f69c
nieveslnormanss@gmail.com <>
2023-04-29 23:56:26 +0800

    ldentilying Our Players

--- Assets/NetworkMove.cs
+++ Assets/NetworkMove.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class NetworkMove : MonoBehaviour
 {
+    struct PostionVector{
+       public float x,y;
+    }
     public Network instance;
     public void OnMove (Vector3 position)
     {
-        Debug.Log("sending postiion to node" + VectorToJson(position));
-        instance.socket.Emit("move",VectorToJson(position));
-        //instance.socket.EmitStringAsJSON("move",VectorToJson(position));
+        string jsonString = JsonUtility.ToJson(VectorToJson(position));
+        Debug.Log("sending postiion to node" +jsonString);
+        //instance.socket.Emit("move",jsonString);
+        instance.socket.EmitStringAsJSON("move",jsonString);
     }
 
-    string VectorToJson (Vector3 vector)
+    PostionVector VectorToJson (Vector3 vector)
     {
-        return string.Format(@"((""x"":""{0}"", ""y"":""{1}""}}", vector.x, vector.z);
+        //return string.Format(@"((""x"":""{0}"", ""y"":""{1}""}}", vector.x, vector.z);
+        PostionVector result = new PostionVector();
+        result.x = vector.x;
+        result.y = vector.z;
+        return result;
     }
 }
